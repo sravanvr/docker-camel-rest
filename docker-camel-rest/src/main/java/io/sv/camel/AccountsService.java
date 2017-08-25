eeb14272962771f94244fe545e64e6081ae1ab77
Sravan Vedala <sravan.vedala@yahoo.com>
2017-08-25 10:44:58 -0700

    Initial commit of Docker-Camel_Rest

---
+++ docker-camel-rest/src/main/java/io/sv/camel/AccountsService.java
@@ -0,0 +1,93 @@
+package io.sv.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.impl.DefaultCamelContext;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class AccountsService {
+
+		 
+	public String getAccountsResposneFromAccountsMicroService(){
+		CamelContext context = new DefaultCamelContext();
+		String ret = null;
+
+	    try {
+	        ProducerTemplate template = context.createProducerTemplate();
+	        context.start();
+
+	        Exchange exchange = template
+	                .request(
+	                		"http://153.83.18.98:1234/AccountInquiry/getAccounts",
+	                        //"http://153.83.18.98:1234/AccountInquiry/getAccounts",
+	                        new Processor() {
+	                            public void process(Exchange exchange)
+	                                    throws Exception {
+	                            }
+	                        });
+
+	        if (null != exchange) {
+	            Message out = exchange.getOut();
+	            ret = out.getBody(String.class);
+	            
+//	            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//	            String json = gson.toJson(ret);
+//	            System.out.println(json);
+//	            ret = json;
+	            System.out.println("Body: " + ret);	//.toString());
+	           
+	        }
+
+	        //Thread.sleep(1000 * 3);
+	        context.stop();
+	        return (ret);
+	        
+	    } catch (Exception ex) {
+	        System.out.println("Here is Exception: " + ex);
+	        return ret;
+	    }
+	}
+	
+
+	public void getResposneFromAccountsMicroService(){
+		CamelContext context = new DefaultCamelContext();
+
+	    try {
+	        ProducerTemplate template = context.createProducerTemplate();
+	        context.start();
+
+	        Exchange exchange = template
+	                .request(
+	                		"https://jsonplaceholder.typicode.com/posts/1",
+	                        //"http://153.83.18.98:1234/AccountInquiry/getAccounts",
+	                        new Processor() {
+	                            public void process(Exchange exchange)
+	                                    throws Exception {
+	                            }
+	                        });
+
+	        if (null != exchange) {
+	            Message out = exchange.getOut();
+	            System.out.println("Body: " + out.getBody(String.class));	//.toString());
+	            int responseCode = out.getHeader(Exchange.HTTP_RESPONSE_CODE,
+	                    Integer.class);
+	            System.out.println("Response: " + String.valueOf(responseCode));
+	        }
+
+	        Thread.sleep(1000 * 3);
+	        context.stop();
+	    } catch (Exception ex) {
+	        System.out.println("Here is Exception: " + ex);
+	    }
+	}
+//	public static void main(String[] args) {
+//		
+//		new AccountsService().getAccountsResposneFromAccountsMicroService();
+//	}
+
+}
